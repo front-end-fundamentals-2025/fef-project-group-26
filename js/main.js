da2e87ab0843d20710c93993cc0aae107f01c18b
Maxliljeqvist <lima24nb@student.ju.se>
2025-03-12 21:53:38 +0100

    fixing the product page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const buttonElement = document.getElementById("button1");
+const inputElement = document.getElementById("input1");
+const textElement = document.getElementById("text1");
+const removeButtonElement = document.getElementById("removeButton1");
+const beigeSofa = document.getElementById("beigeSofa");
+const blueSofa = document.getElementById("blueSofa");
+const beigeSofaButton = document.getElementById("beigeSofaButton");
+
+buttonElement.addEventListener("click", function (e) {});
